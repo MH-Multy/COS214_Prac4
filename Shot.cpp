272b0d07b6060101bc78b0cf8d84d12ef475c4b2
Marko de Swardt <u24658562@tuks.co.za>
2026-09-07 10:28:32 +0300

    Added skeleton cpp files for the productionComponent and SHot class

---
+++ Shot.cpp
@@ -0,0 +1,38 @@
+#include "Shot.h"
+#include "ShotState.h"
+#include <iostream>
+
+Shot::Shot(const std::string& name)
+{
+    
+}
+
+Shot::~Shot()
+{
+    
+}
+
+void Shot::startRecording()
+{
+    
+}
+
+void Shot::stopRecording()
+{
+    
+}
+
+void Shot::setState(ShotState* newState)
+{
+    
+}
+
+const char* Shot::getStateName() const
+{
+    
+}
+
+std::string Shot::getName() const
+{
+    
+}
